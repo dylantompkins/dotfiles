cd7c35b4b18f4def346f72e68ba08e63fc203eb0
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-04-03 21:12:58 -0400

    qtile onedark colors

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -32,6 +32,7 @@ from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
 import subprocess
+import onedark
 
 mod = "mod4"
 terminal = guess_terminal()
@@ -92,20 +93,20 @@ for i in groups:
         Key([mod], i.name, lazy.group[i.name].toscreen(),
             desc="Switch to group {}".format(i.name)),
 
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
+        # # mod1 + shift + letter of group = switch to & move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+        #     desc="Switch to & move focused window to group {}".format(i.name)),
         # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
+        # mod1 + shift + letter of group = move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            desc="move focused window to group {}".format(i.name)),
     ])
 
 layouts = [
     layout.Columns(
         border_focus_stack='#d75f5f',
         margin=8,
-        border_focus='#61afef',
+        border_focus=onedark.colors['blue'],
     ),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
@@ -125,72 +126,41 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font='sans',
-    fontsize=12,
-    padding=3,
+    font='SourceSansPro',
+    fontsize=14,
+    padding=2,
 )
 extension_defaults = widget_defaults.copy()
 
+
+def initMyBar():
+    """
+    Construct my custom bar.
+    """
+    return bar.Bar(
+        [
+            widget.CurrentLayout(),
+            widget.GroupBox(
+                highlight_method='line'
+            ),
+            widget.WindowName(),
+            widget.Systray(),
+            widget.Clock(format='%a %d-%m-%Y %I:%M')
+        ],
+        24,
+        background=onedark.colors['black'],
+        opacity=0.8
+    )
+
 screens = [
     Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
-            ],
-            24,
-        ),
+        bottom=initMyBar()
     ),
     Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
-            ],
-            24,
-        ),
+        bottom=initMyBar()
     ),
     Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
-            ],
-            24,
-        ),
+        bottom=initMyBar()
     ),
 ]
 
---
+++ .config/qtile/onedark.py
@@ -0,0 +1,10 @@
+colors = {
+    'black': '#282c34',
+    'red': '#e06c75',
+    'green': '#98c379',
+    'yellow': '#e5c07b',
+    'blue': '#61afef',
+    'purple': '#c678dd',
+    'cyan': '#56b6c2',
+    'grey': '#abb2bf',
+}
