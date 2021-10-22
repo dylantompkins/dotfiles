1b42a52da0def969d646afdb99d658855fb44843
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-10-21 21:05:06 -0700

    monitors working for login and desktop, given nvidia is enabled

--- .config/lightdm/README.md
+++ .config/lightdm/README.md
@@ -1,3 +1,5 @@
 `lightdm.conf` is just a copy, real located at `/etc/lightdm/lightdm.conf`
 
 maybe symlink in the future?
+
+trimon.sh is my triple monitor setup on the g15. it requires the gpu to be in nvidia mode using optimus
---
+++ .config/lightdm/trimon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0.1 --mode 1680x1050 --pos 2560x1050 --rotate normal --output DP-0.2 --mode 1680x1050 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output eDP-1-1 --primary --mode 2560x1440 --pos 0x341 --rotate normal --output HDMI-1-1 --off
